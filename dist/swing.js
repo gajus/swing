8e5d16f0bfee6cc23e4fe43a479c537bf878ac92
Gajus Kuizinas <gk@anuary.com>
2014-10-29 16:15:09 +0200

    Event listeners can now be attached directly to the card.

--- dist/swing.js
+++ dist/swing.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.7
+ * @version 1.0.8
  * @link https://github.com/gajus/swing for the canonical source repository
  * @license https://github.com/gajus/swing/blob/master/LICENSE BSD 3-Clause
  */
