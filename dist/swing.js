48f0cb96eab5eb2e11c351f18e8915b9df4c4cef
Gajus Kuizinas <gk@anuary.com>
2014-11-03 14:50:52 +0200

    Added stack.getCard() method.

--- dist/swing.js
+++ dist/swing.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.9
+ * @version 1.0.10
  * @link https://github.com/gajus/swing for the canonical source repository
  * @license https://github.com/gajus/swing/blob/master/LICENSE BSD 3-Clause
  */
