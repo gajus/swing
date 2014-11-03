96fa40e1e77fd3681ac3096bce8ce12744fbe977
Gajus Kuizinas <gk@anuary.com>
2014-11-03 17:07:27 +0200

    Test coverage.

--- dist/swing.js
+++ dist/swing.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.0.11
+ * @version 1.0.13
  * @link https://github.com/gajus/swing for the canonical source repository
  * @license https://github.com/gajus/swing/blob/master/LICENSE BSD 3-Clause
  */
