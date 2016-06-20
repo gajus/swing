3ab1cd1961d751e5575dba6a9cecba2cc0e2260e
Stefan <stadolf@gmail.com>
2016-06-20 23:37:04 +0200

    readded webpack configuration. Builds to dist/browser and exports "Swing" as global "library" variable added webpack step to npm build script

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack,
+    filename,
+    plugins;
+
+webpack = require('webpack');
+filename = '[name].js',
+
+    plugins = [
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new webpack.OldWatchingPlugin(),
+        // new webpack.NewWatchingPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.NoErrorsPlugin(),
+    ];
+
+module.exports = {
+    devtool: 'source-map',
+    context: __dirname + '/src',
+    entry: {
+        swing: './index'
+    },
+    output: {
+        path: __dirname + '/dist/browser',
+        filename: filename,
+        library: 'Swing'
+    },
+    plugins: plugins,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/
+                ],
+                loader: 'babel'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '',
+            '.js'
+        ]
+    }
+};
