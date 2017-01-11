bfdc7366dd26427439834398132f6f5a34db3b5e
Gajus Kuizinas <gajus@gajus.com>
2017-01-11 15:19:30 +0200

    feat: remove global gajus. export

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import Card from './Card';
 import Direction from './Direction';
 import Stack from './Stack';
 
-global.gajus = global.gajus || {};
-
-global.gajus.Swing = {
-  Card,
-  Direction,
-  Stack
-};
-
 export {
   Card,
   Direction,
